6cb93134bfbf99dda577d291b6c48935ed06503c
S3nna <61739336+S3nna@users.noreply.github.com>
2021-06-07 23:24:17 -0300

    Delete stonks_maven directory

--- stonks_maven/src/main/java/br/com/stonks/repository/MunicipioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.stonks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.stonks.model.MunicipioModel;
-
-public interface MunicipioRepository extends JpaRepository<MunicipioModel, Integer> {
-
-}
